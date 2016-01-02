b2d3550a69a47e3fb46d50298c2f3053e63975c8
tomorrowdemo <1185111644@qq.com>
2016-01-03 07:55:30 +0800

    BUG修复

--- classProject/public/js/classListNav.js
+++ classProject/public/js/classListNav.js
@@ -1,7 +1,7 @@
        $(document).ready(function(){
            
             $(".course-nav-item").each(function(index){ 
-                    
+                   var index=index+1; 
                 $('#course-nav-item-'+index).click(
                   function(event){
 
@@ -14,7 +14,7 @@
                         error:function(jqXHR){
                         }
                     }); 
-                  for (i=1;i<$('.course-nav-item').length;i++){
+                  for (i=1;i<=$('.course-nav-item').length;i++){
                      if (i==index) {
                         $('.course-nav-item0-'+i).show();
                         $('#course-nav-item-'+i).css('background','red');
@@ -28,10 +28,13 @@
                 });
             });
             $(".courseClass").each(function(index){
-                $(".course-nav-item").each(function(j){
+              var index=index+1; 
+                $(".course-nav-item").each(function(j){ 
+                  var j=j+1;  
                 $('.course-nav-item0-'+j+' #course-'+index).click(
                   function(event){
-                   
+                   console.log(j);
+                   console.log(index);
                     $.ajax({
                         type:"POST",
                         url:"classList?grade="+j+"&classid="+index,
@@ -41,7 +44,7 @@
                         error:function(jqXHR){
                         }
                     }); 
-                  for (i=1;i<$('.courseClass').length;i++){
+                  for (i=1;i<=$('.courseClass').length+1;i++){
                      if (i==index) {
                         
                         $('#course-'+i).css('background','red');
