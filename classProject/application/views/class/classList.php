ae50dfce9f735eca56669fe1adccbbaab776c439
tomorrowdemo <1185111644@qq.com>
2016-01-03 21:33:49 +0800

    小优化

--- classProject/application/views/class/classList.php
+++ classProject/application/views/class/classList.php
@@ -13,7 +13,7 @@
                     </li>
                     <?php foreach($grade as $u=>$v){ ?>
                     <?php foreach($v as $uu=>$vv){ ?>
-                    <li class="course-nav-item <?php if(isset($_GET['grade'])&&$_GET['grade']==$uu) echo("on");?>" id="course-nav-item-<?php echo($uu)?>">
+                    <li class="course-nav-item <?php if(isset($_GET['grade'])&&$_GET['grade']==$uu) echo("on");?>" >
                     <a href="/Classes/classList?grade=<?php echo($uu)?>" ><?php echo($vv);?></a>
                     </li>
                     <?php } ?>
@@ -33,7 +33,7 @@
                             if($_GET['grade']==$u){
                                 foreach ($v as $uu => $vv) { ?>
                             <li class="course-nav-item course-nav-item0-<?php echo($vv['Grade'])?> <?php if(isset($_GET['subject'])&&$_GET['subject']==$vv['SubjectID']) echo("on");?>">
-                            <a href="/Classes/classList?grade=<?php echo($vv['Grade']);?>&subject=<?php echo($vv['SubjectID']);?>"  class="courseClass" id="course-<?php echo($vv['SubjectID']);?>"> <?php echo($vv['Title']);?></a> 
+                            <a href="/Classes/classList?grade=<?php echo($vv['Grade']);?>&subject=<?php echo($vv['SubjectID']);?>" > <?php echo($vv['Title']);?></a> 
                             </li>
                     <?php  }}}}?>    
                 </ul>
@@ -41,29 +41,20 @@
         </div>
     </div>  
     <div class="classShow">
-  	    <div class="classShowNew">
-     		<div class="classShow-title">最新课程</div>
-     		<span><img src="" alt="" /><p>我的</p></span>
-     		<span><img src="" alt="" /><p>我的</p></span>
-     		<span><img src="" alt="" /><p>我的</p></span>
-     		<span><img src="" alt="" /><p>我的</p></span>
-     		<span><img src="" alt="" /><p>我的</p></span>
-     		<span><img src="" alt="" /><p>我的</p></span>
-     		<span><img src="" alt="" /><p>我的</p></span>
-     		<span><img src="" alt="" /><p>我的</p></span>
-     	</div>
-     	<div class="classShowHot">
-     		<div class="classShow-title">热门课程</div>
-     		<span><img src="" alt="" /><p>我的</p></span>
-     		<span><img src="" alt="" /><p>我的</p></span>
-     		<span><img src="" alt="" /><p>我的</p></span>
-     		<span><img src="" alt="" /><p>我的</p></span>
-     		<span><img src="" alt="" /><p>我的</p></span>
-     		<span><img src="" alt="" /><p>我的</p></span>
-     		<span><img src="" alt="" /><p>我的</p></span>
-     		<span><img src="" alt="" /><p>我的</p></span>
+  	    <div class="classShowNav">
+        <?php foreach ($subject as $u => $v) {
+              foreach ($v as $uu => $vv) {?>
+            <a href="/Classes/classList?grade=<?php echo($vv['Grade']);?>&subject=<?php echo($vv['SubjectID']);?>">
+                <span>
+                    <img src="" alt="" />
+                    <p><?php echo($vv['Title']); ?></p>
+                </span>
+            </a>     
+        <?php }} ?>   		
      	</div>
     </div>
+   <?php echo $page_links;?>
+  
 <!--script文件-->
 <!--script type="text/javascript" src="/public/js/classListNav.js"></script-->
 <?php include('/../footer.php');?>
