2ba0fb4d3b0053b8904b71b4aeac647143230b27
tomorrowdemo <1185111644@qq.com>
2016-01-24 20:23:11 +0800

    科目列表查询和分页功能

--- cms/app/controllers/Admin/Subject/List.php
+++ cms/app/controllers/Admin/Subject/List.php
@@ -19,22 +19,23 @@ class Admin_Subject_ListController extends Admin_AbstractController
 
         $page = intval($this->getRequest()->getQuery('page'));
         $page = $page ? $page : 1;
-        $data = array();
+        $data = $count_opt = array();
         $option = array(
             'condition' => '',
             'order' => 'SubjectID asc',
             'limit' => array('offset' => ($page - 1) * $perpage, 'count' => $perpage)
         );
 
+        $data['count'] = Admin_AdminModel::instance()->count($count_opt);
         if (!empty($grade)) {
             $option['condition'] .= "grade = ?";
             $option['bind'] = array($grade);
+            $count_opt = array('condition' => "grade = ?", 'bind' => array($grade));
         }
 
         $data['subjectList'] = Admin_SubjectModel::instance()->fetchAll($option);
-        $data['count'] = Admin_SubjectModel::instance()->count($option);
+        $data['count'] = Admin_SubjectModel::instance()->count($count_opt);
         $data['grade'] = RThink_Config::get('app.grade');
-        // var_dump($data);exit;
         $pagination = new Pagination();
         $data['pagination'] = $pagination->maxnum($data['count'], $perpage)->show('page_metronic');
         $data['menu'] = Widget_Admin_MenuModel::headerMenu();
