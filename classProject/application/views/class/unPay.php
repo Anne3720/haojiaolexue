95986b079d4cfeb9529633c7d450be996622096d
tomorrowdemo <1185111644@qq.com>
2016-02-28 23:12:42 +0800

    视频播放页面

---
+++ classProject/application/views/class/unPay.php
@@ -0,0 +1,6 @@
+<?php include('header.php');?> 
+<link rel="stylesheet" href="/../../../public/css/vedioPlay.css" type="text/css" /> 
+<div class="vedio">
+	<div class="vedioPlay">未支付</div>
+    <div class="vedioContent"></div>
+</div>
--- classProject/application/views/class/vedioPlay.php
+++ classProject/application/views/class/vedioPlay.php
@@ -1,6 +1,6 @@
-<?php include('header.php');?> 
+<?php include('/../header.php');?> 
 <link rel="stylesheet" href="/../../../public/css/vedioPlay.css" type="text/css" /> 
 <div class="vedio">
-	<div class="vedioPlay"></div>
+	<div class="vedioPlay">已支付</div>
     <div class="vedioContent"></div>
 </div>
