6b132729018bbed41462751c5a3818a329307e33
tomorrowdemo <1185111644@qq.com>
2016-03-05 17:54:45 +0800

    课程页面未支付页面

--- classProject/application/views/class/unPay.php
+++ classProject/application/views/class/unPay.php
@@ -1,6 +1,6 @@
 <?php include('header.php');?> 
 <link rel="stylesheet" href="/../../../public/css/unPay.css" type="text/css" /> 
-<div class="vedio">
-	<div class="vedioPlay">尚未购买此课程<a href="/classes/classlist">点此返回课程页面</a></div>
+<div class="vedioPay">
+	<div class="vedioPlay">尚未购买此课程 <a href="#" onClick="javascript :history.back(-1);">点此返回之前页面</a></div>
     <div class="vedioContent"></div>
 </div>
