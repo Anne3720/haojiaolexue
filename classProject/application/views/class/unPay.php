ac7e2b573540e7ee8b1bbd63461e1900de3210bb
tomorrowdemo <1185111644@qq.com>
2016-03-06 14:00:50 +0800

    购买

--- classProject/application/views/class/unPay.php
+++ classProject/application/views/class/unPay.php
@@ -1,14 +1,21 @@
-<?php include('header.php');?> 
+<?php include('/../header.php');?> 
 
 <style type="text/css">
-   *{margin: 0 ;padding: 0;}
-   .vedioPay {
-	font-size: 20px;
-	margin-top: 100px;
-	text-align: center;
-   }
+
+
+*{
+	margin:0;
+	padding:0;
+}
+ul,ol{
+	list-style:none;
+}
+
 </style>
-<div class="vedioPay">
-	<div class="vedioPlay">尚未购买此课程 <a href="#" onClick="javascript :history.back(-1);">点此返回之前页面</a></div>
-    <div class="vedioContent"></div>
+<div>
+	<span>填写并确认订单信息</span>
+	<span>填写并确认订单信息</span>
 </div>
+
+
+	
--- classProject/application/views/class/vedioPlay.php
+++ classProject/application/views/class/vedioPlay.php
@@ -1,15 +1,88 @@
 <?php include('/../header.php');?> 
 <link rel="stylesheet" href="/../../../public/css/vedioPlay.css" type="text/css" />
+<style type="text/css">
+.vedio {
+    width: 1002px;
+    position: relative;
+    height: 610px;
+    margin: 50px auto 50px;
+    background:#D7E7F4; 
+    overflow: hidden;
+ 
+}
+.reback {
+    overflow: hidden;
+    background: #000;
+    width: 100%;
+    height: 40px;
+    color: red;
+    line-height: 40px;
+}
+.reback a {
+    font-size: 16px;
+    color: #317EF3;
+}
+
+.reback:hover a {
+    color:red; 
+}
+.vedioPlay {
+    width: 600px;
+    float: left;
+    padding: 20px;
+    position: relative;
+    overflow: hidden;
+    background: #000;
+    margin:20px; 
+    
+}
+.vedioPlayBox {
+    width: 600px;
+    height: 450px;
+}
+.vedioContent {
+    width: 300px;
+    height: 550px;
+    float: left;
+    overflow: hidden;
+    position: relative;
+    border-left: 2px solid #000;
+}
+.vedioContent span {
+    width: 265px;
+    display:inline-block;
+    padding: 20px 20px;
+    word-wrap: break-word;
+    font-size: 18px;
+}
+.vedioContent span strong {
+    font-size: 18px;
+}
+
+span.time {
+    font-size: 14px;
+    text-align: right;
+    display: block;
+    color: red;
+}
+.bottom {
+    background:#000;
+    width: 100%;
+    height: 40px;
+    position: absolute;
+    bottom: 0px;
+}
+</style>
 <div class="vedio">
     <div class="reback">
-    	<a href="#" onClick="javascript :history.back(-1);"><img src="" alt=""> > 返回</a>
+    	<span class="desc"><strong>课程名称: </strong><?php echo ($Name);?></span>
     </div>
 	<div class="vedioPlay"> 
 	    <video class="vedioPlayBox" src="http://www.cms.com/resource/Video/<?php echo ($Video);?>" controls autobuffer ></video>   
 	    
 	</div>
     <div class="vedioContent">
-    	<span class="desc"><strong>课程名称: </strong><?php echo ($Name);?></span>
+    	
         <span><strong>课程内容: </br></strong></span>
     	<span class="desc"> <?php echo ($Desc);?></span>
     	<span><strong>主讲老师: </strong><?php echo ($Teacher);?></span>
