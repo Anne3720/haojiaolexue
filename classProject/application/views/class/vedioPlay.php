f10fe71667153cd85bfe6159c4ba5fc097ac1b06
tomorrowdemo <1185111644@qq.com>
2016-03-17 04:41:31 +0800

    视频播放器重做

--- classProject/application/views/class/vedioPlay.php
+++ classProject/application/views/class/vedioPlay.php
@@ -1,96 +1,60 @@
 <?php include('/../header.php');?> 
-<link rel="stylesheet" href="/../../../public/css/vedioPlay.css" type="text/css" />
-<style type="text/css">
-.vedio {
-    width: 1002px;
-    position: relative;
-    height: 610px;
-    margin: 50px auto 50px;
-    background:#D7E7F4; 
-    overflow: hidden;
- 
-}
-.reback {
-    overflow: hidden;
-    background: #000;
-    width: 100%;
-    height: 40px;
-    color: red;
-    line-height: 40px;
-}
-.reback a {
-    font-size: 16px;
-    color: #317EF3;
-}
-
-.reback:hover a {
-    color:red; 
-}
-.vedioPlay {
-    width: 600px;
-    float: left;
-    padding: 20px;
-    position: relative;
-    overflow: hidden;
-    background: #000;
-    margin:20px; 
-    
-}
-.vedioPlayBox {
-    width: 600px;
-    height: 450px;
-}
-.vedioContent {
-    width: 300px;
-    height: 550px;
-    float: left;
-    overflow: hidden;
-    position: relative;
-    border-left: 2px solid #000;
-}
-.vedioContent span {
-    width: 265px;
-    display:inline-block;
-    padding: 20px 20px;
-    word-wrap: break-word;
-    font-size: 18px;
-}
-.vedioContent span strong {
-    font-size: 18px;
-}
-
-span.time {
-    font-size: 14px;
-    text-align: right;
-    display: block;
-    color: red;
-}
-.bottom {
-    background:#000;
-    width: 100%;
-    height: 40px;
-    position: absolute;
-    bottom: 0px;
-}
-</style>
-<div class="vedio">
+<link rel="stylesheet" href="/../../../public/css/videoPlay.css" type="text/css" />
+<script type="text/javascript" src="http://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js"></script>
+<script type="text/javascript" src="/../public/js/mediaelement-and-player.min.js"></script>
+<script type="text/javascript">
+// <![CDATA[ 
+$(document).ready(function() { 
+    $('video').mediaelementplayer({ 
+        alwaysShowControls: true, 
+        videoVolume: 'horizontal', 
+        features: ['playpause','progress','volume','fullscreen'] 
+    }); 
+}); 
+// ]]>
+</script>
+<div class="video">
+<div class="container">
     <div class="reback">
-    	<span class="desc"><strong>课程名称: </strong><?php echo ($Name);?></span>
+        <span class="desc"><strong>课程名称: </strong><?php echo ($Name);?></span>
     </div>
-	<div class="vedioPlay"> 
-	    <video class="vedioPlayBox" src="http://www.cms.com/resource/Video/<?php echo ($Video);?>" controls autobuffer ></video>   
-	    
-	</div>
-    <div class="vedioContent">
-    	
+    <div class="videoPlay">    
+        <video width="600" height="450" poster="" controls="controls" preload="none">
+            <!-- MP4 for Safari, IE9, iPhone, iPad, Android, and Windows Phone 7 -->
+            <source type="video/mp4" src="http://www.cms.com/resource/Video/<?php echo ($Video);?>" />
+            <!-- WebM/VP8 for Firefox4, Opera, and Chrome -->
+            <source type="video/webm" src="http://www.cms.com/resource/Video/<?php echo ($Video);?>" />
+            <!-- Ogg/Vorbis for older Firefox and Opera versions -->
+            <source type="video/ogg" src="http://www.cms.com/resource/Video/<?php echo ($Video);?>" />
+            <!-- Optional: Add subtitles for each language -->
+            <track kind="subtitles" src="subtitles.srt" srclang="en" />
+            <!-- Optional: Add chapters -->
+            <track kind="chapters" src="chapters.srt" srclang="en" />
+            <!-- Flash fallback for non-<span class="wp_keywordlink_affiliate"><a href="http://www.dglives.com/        tag/html5" title="查看HTML5中的全部文章" target="_blank">HTML5</a></span> browsers without         JavaScript -->
+            <object width="600" height="450" type="application/x-shockwave-flash" data="flashmediaelement.swf">
+                <param name="movie" value="flashmediaelement.swf" />
+                <param name="flashvars" value="controls=true&file=http://www.cms.com/resource/Video/<?php echo ($Video);?>" />
+            <!-- Image as a last resort -->
+                <img src="myvideo.jpg" width="600" height="450" title="No video playback capabilities" />
+            </object>
+        </video>      
+    </div> 
+    </div>
+   
+    <div class="videoContent">
+        
         <span><strong>课程内容: </br></strong></span>
-    	<span class="desc"> <?php echo ($Desc);?></span>
-    	<span><strong>主讲老师: </strong><?php echo ($Teacher);?></span>
-    	<span class="desc" style="font-family: 'Microsoft YaHei';">课程优惠价：<?php echo "&yen; "; echo ($Price);?></span>
+        <span class="desc"> <?php echo ($Desc);?></span>
+        <span><strong>主讲老师: </strong><?php echo ($Teacher);?></span>
+        <span class="desc" style="font-family: 'Microsoft YaHei';">课程优惠价：<?php echo "&yen; "; echo ($Price);?></span>
         <span class="time">更新时间:<?php echo ($UpdateTime);?></span>
     </div>
     <div class="bottom">
         
     </div>
+</div>   
 </div>
+
+
+
 <?php include('/../footer.php');?>
---
+++ classProject/public/js/videoPlayer.js
@@ -0,0 +1,7 @@
+
+(function(window, document) { 
+   
+    
+
+}( this, document ))
+
