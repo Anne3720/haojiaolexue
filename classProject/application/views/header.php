6332a9d5748ed8a5f4dc22985f72db78feec19b7
SpikeLavender <hetengjiao_xjtu@163.com>
2017-02-20 17:18:21 +0800

    首页jS分离
    将所用到的js分离开来，后续会进行打包处理，增加运行速度

--- classProject/application/views/header.php
+++ classProject/application/views/header.php
@@ -1,39 +1,18 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1.0" />
-	  <title>好教乐学</title>
-	  <link rel="stylesheet" href="/public/css/main.css" type="text/css" />
-    <script type="text/javascript" src="http://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js"></script>
-    
-    <script src="/public/js/bootstrap-transition.js"></script>
-    <script src="/public/js/bootstrap-modal.js"></script>
-    <script type="text/javascript" src="/public/easyUI/jquery.easyui.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="/public/easyUI/themes/default/easyui.css">
-    <link rel="stylesheet" type="text/css" href="/public/easyUI/themes/icon.css">
-    <link rel="stylesheet" type="text/css" href="/public/easyUI/demo/demo.css">
-    
-</head>
-<body>
+
     <div class="navtop">
-       <div class="navtop-logo"><img width="230px" src="/public/img/logo.png" alt="" /></div>
-       <div class="navtop-content">
+       <div class="navtop-logo col-md-2" ><img width="100%" height="100%" src="/public/img/logo.png" alt="" /></div>
+       <div class="navtop-content col-md-10" >
        	    <ul class="navtop-class">
-        	    <li><a href="/classes/classList">课程</a></li>
-            </ul>
-            <ul class="navtop-login">
+        	    <li class="navtop-class col-md-2"><a href="/classes/classList">课程</a></li>
             <?php if(!isset($_SESSION['userInfo'])&&empty($_SESSION['userInfo'])){ ?>
-		        <li class="btnlogin-reg-login" data-toggle="modal" data-target="#modal">登录</li>
-		        <li class="btnlogin-reg-reg" data-toggle="modal" data-target="#modal">注册</li>
-	        </ul>
-	        <?php }else{ ?>
-	        <ul class="navtop-login">
-	            <li><a href="" >学习中心</a></li>
-	            <li><a href="/user/information" >个人信息</a></li>
-		        <li><a href="/user/doLogout" >退出</a></li>
+		            <li class="col-md-2 col-md-offset-6 btnlogin-reg-login" data-toggle="modal" data-target="#modal">登录</li>
+		            <li class="col-md-2 btnlogin-reg-reg" data-toggle="modal" data-target="#modal">注册</li>
+	          <?php }else{ ?>
+	            <li class="col-md-2 col-md-offset-4"><a href="" >学习中心</a></li>
+	            <li class="col-md-2 col-md-offset-0"><a href="/user/information" >个人信息</a></li>
+		          <li class="col-md-2 col-md-offset-0"><a href="/user/doLogout" >退出</a></li>
 		    <?php } ?>
-	        </ul>
+	          </ul>
        </div>
         
 	    
@@ -41,13 +20,12 @@
   <div id="modal" class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-sm">
             <div class="modal-content">
-              
-              <?php include('login-reg.php');?> 
-                    
+              <?php include('login-reg.php');?>                    
             </div>
         </div>
     </div>
-   
+
+  
 
 
 
